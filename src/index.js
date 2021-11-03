eac014b37535763293f312564f62aa56df326dfd
emilio-v <emilio_785@hotmail.com>
2021-11-03 00:25:08 -0600

    removed unusable code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// import App from './App';
 import CounterApp from './CounterApp';
 
 ReactDOM.render(<CounterApp value={0} />, document.getElementById('root'));
